e5d6de4bcc750512fd909238a7139cc34e0d172d
diego.taub <diego.taub@accenture.com>
2021-04-04 21:49:57 -0300

    Ejercicio26

---
+++ Ejercicio26/Ejercicio26/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Fecha;
+            while (true)
+            {
+                Console.Write("Ingrese una fecha: ");
+                if(DateTime.TryParse(Console.ReadLine(), out Fecha))
+                {
+                    break;
+                }
+                Console.WriteLine("Fecha errónea.");
+            }
+            Console.WriteLine($"Día ingresado: {Fecha.Day}");
+            Console.WriteLine($"Nombre de día ingresado: {Fecha.DayOfWeek}");
+            Console.WriteLine($"Número de día de la semana ingresado: {Convert.ToInt32(Fecha.DayOfWeek)}");
+            Console.WriteLine($"Mes ingresado: {Fecha.Month}");
+            Console.WriteLine($"Nombre de Mes ingresado: {Fecha.ToString("MMMM")}");
+            Console.WriteLine($"Año ingresado: {Fecha.Year}");
+            Console.ReadKey();
+        }
+    }
+}
